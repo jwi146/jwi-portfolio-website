a29483e17bd621b850923ec3b373bf19f4bb9738
jwi146 <james24146@gmail.com>
2026-08-17 19:34:22 +0100

    terminal reveal being implemented

---
+++ src/components/TerminalReveal.jsx
@@ -0,0 +1,11 @@
+import {useState, useEffect} from 'react'
+
+//reveal a terminal styled box with a few bullet points about me
+const TerminalReveal = () =>{
+
+    const[isopen, setIsOpen] = useState(false);
+    const [displayedtext, setDisplayedText] = useState("");
+
+    //text to be displayed
+    const fullText = "...";
+}
