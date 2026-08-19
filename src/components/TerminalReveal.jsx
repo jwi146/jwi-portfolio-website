f16256b4072ea2da3e7d1f52f19e212b52ec4139
jwi146 <james24146@gmail.com>
2026-08-19 18:07:15 +0100

    add shadow to button

--- src/components/TerminalReveal.jsx
+++ src/components/TerminalReveal.jsx
@@ -39,7 +39,7 @@ const TerminalReveal = () => {
     return (
         <div className="text-center">
             <button
-                className="bg-gray-500 text-white px-4 py-2 rounded font-mono"
+                className="bg-gray-500 text-white px-4 py-2 rounded font-mono shadow"
                 onClick={handleToggle}
             >
                 &gt;_whoami
