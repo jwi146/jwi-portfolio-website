f92e5b69b3de77f6b1f9317d90f3d18ee2b8c2d7
jwi146 <james24146@gmail.com>
2026-08-20 15:38:19 +0100

    added more background colours

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -2,8 +2,10 @@
 const Background = () => {
     return (
         <div className="fixed inset-0 -z-10 overflow-hidden pointer-events-none">
-            <div className="absolute w-[600px] h-[600px] rounded-full bg-rose-950/20 blur-3xl top-[-10%] left-[-10%] animate-drift-slow" />
-            <div className="absolute w-[500px] h-[500px] rounded-full bg-amber-400/15 blur-3xl bottom-[-10%] right-[-5%] animate-drift-slow-reverse" />
+            <div className="absolute w-[600px] h-[600px] rounded-full bg-red-600/20 blur-3xl top-[-10%] left-[-10%] animate-drift-slow-right" />
+            <div className="absolute w-[500px] h-[500px] rounded-full bg-amber-400/15 blur-3xl bottom-[-10%] right-[-10%] animate-drift-slow-reverse" />
+            <div className="absolute w-[500px] h-[500px] rounded-full bg-green-300/15 blur-3xl bottom-[-10%] left-[-5%] animate-drift-slow-up" />
+            <div className="absolute w-[500px] h-[500px] rounded-full bg-purple-300/15 blur-3xl top-[-10%] right-[-5%] animate-drift-slow-down" />
         </div>
     )
 }
